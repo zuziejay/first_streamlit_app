2fc533b6f7b87cfca5d733cdfcf728f983df8567
Zuzana Jankovská <77232242+zuziejay@users.noreply.github.com>
2023-09-12 16:55:13 +0200

    Update streamlit_app.py
    removing unnecessary json line

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,6 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
 # take the json version of the version and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
