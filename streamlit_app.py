6b873781fec04410edf2cfa5eb0054795344b64e
Zuzana Jankovská <77232242+zuziejay@users.noreply.github.com>
2023-09-12 14:31:44 +0200

    Update streamlit_app.py
    fix of quotation marks

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents' New Healthy Diner')
+streamlit.title("My Parents' New Healthy Diner")
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
