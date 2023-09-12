65ecc226fb69ea3fe3742380a3912de362a04601
Zuzana Jankovská <77232242+zuziejay@users.noreply.github.com>
2023-09-12 16:52:54 +0200

    Update streamlit_app.py
    adding request header
    changing request output to json

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ import requests
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 streamlit.title("My Parents' New Healthy Diner")
 
@@ -23,4 +22,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-streamlit.text(fruityvice_response)
+streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
