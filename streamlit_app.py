6a2f30c52b2a26ae00939bbcaeb7f3ffdad18d83
Zuzana Jankovská <77232242+zuziejay@users.noreply.github.com>
2023-09-13 10:57:38 +0200

    Update streamlit_app.py
    adding insert into snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,6 @@ streamlit.dataframe(my_data_rows)
 # Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+# writing to snowflake
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
