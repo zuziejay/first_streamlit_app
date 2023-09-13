144e8a84f741199cdf7fde4d46e22830962cb0df
Zuzana Jankovská <77232242+zuziejay@users.noreply.github.com>
2023-09-13 11:07:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get(''.join("https://fruityvice.com/api/fruit/",fruit_choice))
+fruityvice_response = requests.get(''.join(["https://fruityvice.com/api/fruit/",fruit_choice]))
 
 # take the json version of the version and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
